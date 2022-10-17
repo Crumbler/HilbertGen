e662f5d89f29ea925583c5af7828c443622eb72e
Crumbler <25861574+Crumbler@users.noreply.github.com>
2022-10-18 01:53:01 +0300

    Add project files.

---
+++ FormMain.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace HilbertGen
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
